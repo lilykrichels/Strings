5cdb23d13ac159f8e9ebc167d3eed1b3efdc807f
lilykrichels <lily.krichels@gmail.com>
2017-08-22 20:50:10 -0400

    more problems

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -14,33 +14,26 @@ namespace Strings
             //string phrase = Console.ReadLine();
             //Console.WriteLine("This word is " + phrase.Length + " characters long.");
 
-            string firstName = "Lily";
-            string lastName = "Krichels";
-            string fullName = string.Concat(firstName, " ", lastName);
 
-            Console.WriteLine("Your initials are " + firstName[0] + lastName[0]);
-            Console.WriteLine(firstName[firstName.Length -1]); //this will return the last letter, the index of the last letter and the last letter are different 
-            Console.WriteLine(firstName.Equals(lastName));
-            Console.WriteLine(fullName == lastName);
-            Console.WriteLine();
-            Console.WriteLine("My favorite book is \"A Guide to the Good Life\"");
+            //Ask the user to guess the type of pet(Hint to the user it's the opposite of a dog)
+            //Assign an answer variable to the answer of cat.
+            //If they guess cat correctly, print "You guessed correctly!"
+            //If they guess incorrectly, print "You guessed incorrectly..."
+            //Use the ToLower() or ToUpper() methods to ensure if they enter cat or CAT or Cat, it will be correct.
 
-            string example = "oneTwoThree";
-            //substring takes a start, index 0 and a length, 6 indxes long
-            string sub = example.Substring(3, 8);
-            Console.WriteLine(sub);
+            Console.WriteLine("Guess the type of animal I'm thinking of: ");
+            string answer = "Cat";
+            string userGuess = Console.ReadLine();
+             if (userGuess.ToUpper() == answer.ToUpper())
+            {
+                Console.WriteLine("You guess correctly!");
+            }
+            else
+            {
+                Console.WriteLine("Please try again.");
+            }
 
-            string index = "Mackelmore";
-            string result = index.Remove(3);
-            Console.WriteLine( result);
-            Console.WriteLine(fullName.IndexOf("l"));
 
-            string yell = "      \n\n\n    LILY";
-            string newYell = yell.Trim();
-            Console.WriteLine(yell);
-            Console.WriteLine(newYell);
-
-           
         }
     }
 }
